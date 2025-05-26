e100d4ff1feff798f13b4dfbc11eef33753ac756
Piep220 <pfallon05@gmail.com>
2025-05-26 12:00:59 +0100

    CH4 L3 incomplete

--- src/block_converter.py
+++ src/block_converter.py
@@ -1,4 +1,10 @@
 from enum import Enum
+from htmlnode import HTMLNode
+from converters import text_node_to_html_node
+from textnode import TextType, TextNode
+from markdown_converter import text_to_textnodes
+from parentnode import ParentNode
+from leafnode import LeafNode
 
 class BlockType(Enum):
     PARA = "paragraph"
@@ -49,3 +55,51 @@ def block_to_block_type(block:str):
                 return BlockType.PARA
         return BlockType.ORDERED
     return BlockType.PARA
+
+def text_to_children(text:str):
+    match block_to_block_type(text):
+        #https://developer.mozilla.org/en-US/docs/Learn_web_development/Core/Structuring_content/Lists
+        case BlockType.PARA:
+            return block_convert_para(text)
+        case BlockType.HEAD:
+            return HTMLNode("h1", text)
+        case BlockType.QUOTE:
+            return block_convert_quote(text)
+        case BlockType.CODE:
+            leaf = text_node_to_html_node(TextNode(text[3:-3], TextType.CODE))
+            return LeafNode("pre", leaf)
+        case BlockType.UNORDERED:
+            return HTMLNode("ul", text)
+        case BlockType.ORDERED:
+            return HTMLNode("ol", text)
+
+def block_convert_para(text:str):
+    text_nodes = text_to_textnodes(text)
+    leaf_nodes = []
+    for node in text_nodes:
+        leaf_nodes.append(text_node_to_html_node(node))
+    return ParentNode("p", leaf_nodes)
+
+def block_convert_head(text:str):
+    pass
+
+def block_convert_quote(text:str):
+    text_nodes = text_to_textnodes(text)
+    leaf_nodes = []
+    for node in text_nodes:
+        leaf_nodes.append(text_node_to_html_node(node))
+    return ParentNode("blockquote", text_nodes)
+
+def block_convert_unordered(text:str):
+    pass
+
+def block_convert_ordered(text:str):
+    pass
+
+def markdown_to_html_node(markdown:str):
+    blocks = markdown_to_blocks(markdown)
+    nodes = []
+    for block in blocks:
+        nodes.append(text_to_children(block))
+
+    return ParentNode("div", nodes)
--- src/main.py
+++ src/main.py
@@ -33,8 +33,8 @@ def main():
     #print(split_nodes_image([node]))
     #print(type(node))
     #print(type([node]))
-    text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
-    pprint(text_to_textnodes("None"))
+    #text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+    #pprint(text_to_textnodes("None"))
     markdown = """# This is a heading
 
 This is a paragraph of text. It has some **bold** and _italic_ words inside of it.
@@ -44,10 +44,34 @@ This is a paragraph of text. It has some **bold** and _italic_ words inside of i
 - This is a list item
 - This is another list item      
 """
-    test = markdown_to_blocks(markdown)
-    pprint(markdown)
-    print(type(test), len(test))
-    print(test)
+    #test = markdown_to_blocks(markdown)
+    #pprint(markdown)
+    #print(type(test), len(test))
+    #print(test)
+    md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+    print(md)
+    print("<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>")
+    pprint(markdown_to_html_node(md))
+    md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+    print("###########################")
+    print(md)
+    print("<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>")
+    test = markdown_to_html_node(md)
+    pprint(test.to_html())
+    #for node in test.children:
+    #    print(type(node))
 
 main()
 
